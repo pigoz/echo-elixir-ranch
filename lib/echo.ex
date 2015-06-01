3009f426e636d6f80ebec914efa41020726f580b
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2015-06-01 21:22:35 +0200

    cosmetics

--- lib/echo.ex
+++ lib/echo.ex
@@ -3,10 +3,13 @@ require Logger
 defmodule Echo do
   use Application
 
+  @options  [port: 5555]
+  @protocol EchoProtocol
+
   def start(_type, _args) do
     Logger.info("[echo]#start")
-    { :ok, _ } = :ranch.start_listener(:tcp_echo, 1,
-      :ranch_tcp, [port: 5555], EchoProtocol, [])
+    { :ok, _ } = :ranch.start_listener(
+      :tcp_echo, 1, :ranch_tcp, @options, @protocol, [])
     EchoSup.start_link
   end
 
